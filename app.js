843f119b2498d51a3504ae9e161932e9b8eaac38
zheng dongcheng <773770532@qq.com>
2019-01-03 17:32:25 +0800

    --

--- app.js
+++ app.js
@@ -4,39 +4,39 @@ App({
   onLaunch(options) {
     // 第一次打开
     options.query == {number:1}
-    // const that = this;
-         my.switchTab({
-              url: '../index/index'
+    const that = this;
+        //  my.switchTab({
+        //       url: '../index/index'
+        //       })
+    my.getAuthCode({
+      scopes: 'auth_user',
+      success: (res) => {
+        if (res.authCode){ 
+          my.httpRequest({
+            url: authServlet, // 目标服务器url 
+            data:{
+              authcode: res.authCode
+            },
+            method: 'POST',
+            success: (res) => {
+              console.log(res)
+              // that.setData({})
+              that.appuserinfo.username = res.data[0].username;
+              that.appuserinfo.aid = res.data[0].aid;
+              my.switchTab({
+                url: '../index/index'
               })
-    // my.getAuthCode({
-    //   scopes: 'auth_user',
-    //   success: (res) => {
-    //     if (res.authCode){ 
-    //       my.httpRequest({
-    //         url: authServlet, // 目标服务器url 
-    //         data:{
-    //           authcode: res.authCode
-    //         },
-    //         method: 'POST',
-    //         success: (res) => {
-    //           console.log(res)
-    //           // that.setData({})
-    //           that.appuserinfo.username = res.data[0].username;
-    //           that.appuserinfo.aid = res.data[0].aid;
-    //           my.switchTab({
-    //             url: '../index/index'
-    //           })
-    //         },
-    //         fail:() => {
-    //           my.alert({
-    //             title: '提示',
-    //             content:'连接服务器失败，请稍后再试！'
-    //           });
-    //         },
-    //       });
-    //     }
-    //   },
-    // });
+            },
+            fail:() => {
+              my.alert({
+                title: '提示',
+                content:'连接服务器失败，请稍后再试！'
+              });
+            },
+          });
+        }
+      },
+    });
     console.info('App onLaunch');
     
   },
